a822326636ba645436d498f999d9f962627d197a
Josh Liuaana <jfliuaana@gmail.com>
2023-04-27 14:07:33 +1200

    add mock data to keep test files cleaner

---
+++ models/mock-data.ts
@@ -0,0 +1,62 @@
+export const mockDb = [
+  {
+    id: 1,
+    title: 'Big Hero 6',
+    imdb_id: 'tt2245084',
+    watched: false,
+    img: 'https://m.media-amazon.com/images/M/MV5BMDliOTIzNmUtOTllOC00NDU3LWFiNjYtMGM0NDc1YTMxNjYxXkEyXkFqcGdeQXVyNTM3NzExMDQ@._V1_Ratio0.7027_AL_.jpg',
+    date_added: 1682205263111,
+  },
+  {
+    id: 2,
+    title: 'Luca',
+    imdb_id: 'tt12801262',
+    watched: false,
+    img: 'https://m.media-amazon.com/images/M/MV5BZTQyNTU0MDktYTFkYi00ZjNhLWE2ODctMzBkM2U1ZTk3YTMzXkEyXkFqcGdeQXVyNTI4MzE4MDU@._V1_Ratio0.6757_AL_.jpg',
+    date_added: 1682205263113,
+  },
+  {
+    id: 3,
+    title: 'Inside Out',
+    imdb_id: 'tt2096673',
+    watched: false,
+    img: 'https://m.media-amazon.com/images/M/MV5BOTgxMDQwMDk0OF5BMl5BanBnXkFtZTgwNjU5OTg2NDE@._V1_Ratio0.6757_AL_.jpg',
+    date_added: 1682205263115,
+  }
+]
+
+export const ogMockDb = [
+  {
+    id: 1,
+    title: 'Big Hero 6',
+    imdb_id: 'tt2245084',
+    watched: false,
+    img: 'https://m.media-amazon.com/images/M/MV5BMDliOTIzNmUtOTllOC00NDU3LWFiNjYtMGM0NDc1YTMxNjYxXkEyXkFqcGdeQXVyNTM3NzExMDQ@._V1_Ratio0.7027_AL_.jpg',
+    date_added: 1682205263111,
+  },
+  {
+    id: 2,
+    title: 'Luca',
+    imdb_id: 'tt12801262',
+    watched: false,
+    img: 'https://m.media-amazon.com/images/M/MV5BZTQyNTU0MDktYTFkYi00ZjNhLWE2ODctMzBkM2U1ZTk3YTMzXkEyXkFqcGdeQXVyNTI4MzE4MDU@._V1_Ratio0.6757_AL_.jpg',
+    date_added: 1682205263113,
+  },
+  {
+    id: 3,
+    title: 'Inside Out',
+    imdb_id: 'tt2096673',
+    watched: false,
+    img: 'https://m.media-amazon.com/images/M/MV5BOTgxMDQwMDk0OF5BMl5BanBnXkFtZTgwNjU5OTg2NDE@._V1_Ratio0.6757_AL_.jpg',
+    date_added: 1682205263115,
+  }
+]
+
+export const movieFromDb = {
+  id: 6,
+  title: 'Cars',
+  imdb_id: 'tt0317219',
+  watched: 0,
+  img: 'https://m.media-amazon.com/images/M/MV5BMTg5NzY0MzA2MV5BMl5BanBnXkFtZTYwNDc3NTc2._V1_Ratio0.6757_AL_.jpg',
+  date_added: 1682205263129
+}
