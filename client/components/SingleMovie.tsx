d34f9f80178b16e96ec80d49e10c05d302b9f5ca
Josh Liuaana <jfliuaana@gmail.com>
2023-05-02 11:34:30 +1200

    fix key down tabbing

--- client/components/SingleMovie.tsx
+++ client/components/SingleMovie.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch } from "../hooks/redux"
 
 import { Movie } from "../../models/movies"
 import { deleteMovieThunk, updateSeenThunk } from "../actions/movies"
+import { KeyboardEvent } from "react"
 
 interface Props {
   movie: Movie
@@ -24,16 +25,28 @@ function SingleMovie({ movie }: Props) {
     dispatch(updateSeenThunk(id, seen))
   }
 
+  const handleKeyDownWatched = (evt: KeyboardEvent, id: number, seen: boolean) => {
+    if (evt.key === 'Enter') {
+      handleWatched(id, seen)
+    }
+  }
+
+  const handleKeyDownDelete = (evt: KeyboardEvent, id: number) => {
+    if(evt.key === 'Enter') {
+      handleDelete(id)
+    }
+  } 
+
   return (
     <>
       <div className="movie">
         <div className="movie-img-container">
           {movie.watched
-            ? <i className="movie-butt fa-regular fa-eye fa-xl" tabIndex={0} onKeyDown={() => {handleWatched(movie.id, false)}} style={{color: "#006400"}} role="button" onClick={() => handleWatched(movie.id, false)}></i>
-            : <i className="movie-butt fa-regular fa-eye-slash fa-xl" tabIndex={0} onKeyDown={() => {handleWatched(movie.id, true)}} style={{color: "#9e0000"}} role="button" onClick={() => handleWatched(movie.id, true)}></i>
+            ? <i className="movie-butt fa-regular fa-eye fa-xl" tabIndex={0} onKeyDown={(evt) => {handleKeyDownWatched(evt, movie.id, false)}} style={{color: "#006400"}} role="button" onClick={() => handleWatched(movie.id, false)}></i>
+            : <i className="movie-butt fa-regular fa-eye-slash fa-xl" tabIndex={0} onKeyDown={(evt) => {handleKeyDownWatched(evt, movie.id, true)}} style={{color: "#9e0000"}} role="button" onClick={() => handleWatched(movie.id, true)}></i>
           }
           <h2 className="movie-title">{movie.title}</h2>
-          <i className="movie-butt fa-solid fa-trash fa-xl" tabIndex={0} onKeyDown={() => {handleDelete(movie.id)}} role="button" style={{color: "#9e0000"}} onClick={() => handleDelete(movie.id)} />
+          <i className="movie-butt fa-solid fa-trash fa-xl" tabIndex={0} onKeyDown={(evt) => {handleKeyDownDelete(evt, movie.id)}} role="button" style={{color: "#9e0000"}} onClick={() => handleDelete(movie.id)} />
         </div>
         <img src={movie.img} alt={`movie poster for ${movie.title}`}/>
         <button className="button info-button" onClick={() => handleDetails(movie.imdb_id)}>Movie Details</button>
